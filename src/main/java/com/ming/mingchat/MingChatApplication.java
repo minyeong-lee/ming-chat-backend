982a02ef1bc3f0ba78902da458ad49ec64d5a9cf
minyeong-lee <lmy1366@gmail.com>
2025-05-18 02:55:50 +0900

    first commit

---
+++ src/main/java/com/ming/mingchat/MingChatApplication.java
@@ -0,0 +1,13 @@
+package com.ming.mingchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/mingchat/MingChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.mingchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
